f51c07e253a372d85b8df62991d556f39fc74406
KirillKazak <kazzak.kirill1995@gmail.com>
2022-08-28 17:41:49 +0300

    create useCase classes

---
+++ app/src/main/java/kazak/kirill/shoplist1/domain/GetShopItemFromIdUseCase.kt
@@ -0,0 +1,8 @@
+package kazak.kirill.shoplist1.domain
+
+class GetShopItemFromIdUseCase {
+
+    fun getShopItemFromIdUseCase(shopItemId: Int): ShopItem {
+        TODO()
+    }
+}
---
+++ app/src/main/java/kazak/kirill/shoplist1/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package kazak.kirill.shoplist1.domain
+
+class GetShopListUseCase {
+
+    fun getShopList(): List<ShopItem> {
+        TODO()
+    }
+}
